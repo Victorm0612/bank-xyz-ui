a327f3d4bd91ff2aba4e57837a75d8a032fb3c00
Victor Manuel Vargas Rojas <49686607+Victorm0612@users.noreply.github.com>
2022-02-19 14:20:14 -0500

    services and servicesMenu components created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import HomeComponent from './components/HomeComponent';
 import NavbarComponent from './components/NavbarComponent';
+import ServiceComponent from './components/Services/ServiceComponent';
+import ServicesMenuComponent from './components/Services/ServicesMenuComponent';
+import NotFoundedComponent from './components/UI/NotFoundedComponent';
 import ToastComponent from './components/UI/ToastComponent';
 
 const App = () => (
@@ -10,7 +14,12 @@ const App = () => (
     <NavbarComponent>
       <h1 className="home-title text-center">¡Bienvenido a XYZ Bank!</h1>
     </NavbarComponent>
-    <HomeComponent />
+    <Routes>
+      <Route path="/" exact element={<HomeComponent />} />
+      <Route path="/services" element={<ServicesMenuComponent />} />
+      <Route path="/services/:id" element={<ServiceComponent />} />
+      <Route path="*" element={<NotFoundedComponent />} />
+    </Routes>
   </div>
 );
 
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoImg from '../assets/img/logo.png';
 import './NavbarComponent.scss';
 
 const NavbarComponent = ({ children }) => (
   <header className="nav justify-content-center">
-    <img src={logoImg} alt="logo-bank-xyz" width="60" height="auto" />
+    <Link to="/">
+      <img src={logoImg} alt="logo-bank-xyz" width="60" height="auto" />
+    </Link>
     {children}
   </header>
 );
